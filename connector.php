c142f24ded3e4774b4ec7d785421a9c33318df8e
theodosisathanasakis <theathral@csd.auth.gr>
2020-01-09 12:32:55 +0200

    small changes

--- connector.php
+++ connector.php
@@ -1,3 +1,3 @@
 <?php
-$connector = mysqli_connect("155.207.1.6", "theathral1", "theathral1", "elearning");
+$connector = mysqli_connect("localhost", "theathral1", "theathral1", "elearning");
 if (!$connector) die("Couldn't connect to the database " . mysqli_error());
